3cd19abeb3cf4c02f0cbc142c516453ffd3813be
YukiZensho <yukizenshou@gmail.com>
2022-12-03 03:37:51 +0200

    Basic communication

---
+++ cli/src/main.c
@@ -0,0 +1,103 @@
+
+/*
+ *      THIS IS THE CLIENT
+*/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdarg.h>
+#include <netdb.h>
+
+#include <curses.h>
+
+#define BUFFER_SIZE 0x800
+
+void error(const char *output, ...){/*{{{*/
+
+    va_list vList;
+    va_start(vList, output);
+    
+    printf("%s", output);
+
+    int errorValue = va_arg(vList, int);
+    if(errorValue)
+        exit(errorValue);
+    exit(-1);
+}/*}}}*/
+
+int main(int argC, char * argV[]){/*{{{*/
+
+    if(argC < 3){
+        char * output;
+        output = malloc(strlen(argV[0])+20);
+        strcpy(output, argV[0]);
+        strcat(output, " [address] [port]");
+        error(output, 1);
+        free(output);
+    }
+
+    initscr();
+
+    cbreak();
+    noecho();
+    nodelay(stdscr, TRUE);
+    scrollok(stdscr, TRUE);
+    int keypress;
+
+
+/*{{{ Socket configuration*/    
+    int socketFD, portNumber;
+    char buffer[BUFFER_SIZE];
+
+    struct sockaddr_in servAddr;
+    struct hostent * server;
+
+    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if(socketFD < 0)
+        error("socket opening error", 2);
+    portNumber = atoi(argV[2]);
+    server = gethostbyname(argV[1]);
+    if(server == NULL)
+        error("can not connect to host", -1);
+
+    bzero((char *) &servAddr, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *) &servAddr.sin_addr.s_addr, server->h_length);
+    servAddr.sin_port = htons(portNumber);
+
+    if(connect(socketFD, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0)
+        error("connection error", 3);
+/*}}}*/
+    while(1){
+        keypress = getch();
+        if(!keypress || keypress == ERR)
+            continue;
+        if(write(socketFD, &keypress, sizeof(keypress)) < 0)
+            error("write error", 4);
+/*      bzero(buffer, BUFFER_SIZE);
+        fgets(buffer, BUFFER_SIZE, stdin);
+
+        if(write(socketFD, buffer, strlen(buffer)) < 0)
+            error("write error", 4);
+
+        bzero(buffer, BUFFER_SIZE);
+        if(read(socketFD, buffer, BUFFER_SIZE) < 0)
+            error("read error", 5);
+
+        printf("Server: %s", buffer);
+        if(strncmp(buffer, "exit", 4) == 0)
+            break;
+*/
+        }
+    printf("DONE!\n");
+
+    close(socketFD);
+    return 0;
+}/*}}}*/
---
+++ srv/src/main.c
@@ -0,0 +1,100 @@
+
+/*
+ *      THIS IS THE SERVER
+*/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdarg.h>
+
+
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+
+#define BUFFER_SIZE 0x800
+#define NUMBER_OF_USERS 2
+
+void error(const char *output, ...){/*{{{*/ 
+
+    //triple brakets represent a fold in nvim if you set foldmethod=marker
+
+    va_list vList;
+    va_start(vList, output);
+    
+    printf("%s", output);
+
+    int errorValue = va_arg(vList, int);
+    if(errorValue)
+        exit(errorValue);
+    exit(-1);
+}/*}}}*/
+
+
+int main(int argC, char *argV[]){/*{{{*/
+    initscr();        
+    if(argC < 2){
+        char * output;
+        output = malloc(strlen(argV[0])+10);
+        strcpy(output, argV[0]);
+        strcat(output, " [port]");
+        error(output, 1);
+        free(output);
+    }
+    
+    int socketFD, newSocketFD, portNumber;
+    char buffer[BUFFER_SIZE];
+
+    struct sockaddr_in servAddr, cliAddr;
+    socklen_t cliLen;
+
+    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if(socketFD < 0)
+        error("socket opening error", 2);
+
+    bzero((char *) &servAddr, sizeof(servAddr));
+    bzero((char *) &cliAddr, sizeof(cliAddr));
+    portNumber = atoi(argV[1]);
+    
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = INADDR_ANY;
+    servAddr.sin_port = htons(portNumber);
+
+    if(bind(socketFD, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0)
+        error("binding error", 3);
+
+    listen(socketFD, NUMBER_OF_USERS);
+    cliLen = sizeof(cliAddr);
+
+    newSocketFD = accept(socketFD, (struct sockaddr *) &cliLen, &cliLen);
+
+    if(newSocketFD < 0)
+        error("socket accept error", 4);
+    int readV, writeV;
+
+    // Herein layeth the logic
+    while(1){
+        bzero(buffer, BUFFER_SIZE);
+        if((readV = read(newSocketFD, buffer, BUFFER_SIZE)) < 0)
+                error("Reading error", 5);
+        printf("Client: %s\n", buffer);
+
+        bzero(buffer, BUFFER_SIZE);
+        fgets(buffer, BUFFER_SIZE, stdin);
+
+        if((write(newSocketFD, buffer, strlen(buffer))) < 0)
+                error("Writing error", 6);
+
+        if(strncmp(buffer, "exit", 4) == 0)
+            break;
+   }
+   close(newSocketFD);
+   close(socketFD);
+   return 0;
+}/*}}}*/
